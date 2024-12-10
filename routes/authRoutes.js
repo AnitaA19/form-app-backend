2eba0d1367d944ecfc89fa7b5b7b5f7237a932fe
ANITA\anita <balasanyananita@gmail.com>
2024-12-11 00:46:21 +0400

    fix: fixed router

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
-// routes/authRoutes.js
 const express = require('express');
+const loginRouter = require('../controllers/login');
+const registerRouter = require('../controllers/register');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({ message: "API is working" });
-});
+router.use('/login', loginRouter);
+router.use('/register', registerRouter);
 
 module.exports = router;
