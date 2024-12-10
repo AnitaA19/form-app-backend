17898d88df331bb6c2d8c25bd7ca9f1fc09a792a
ANITA\anita <balasanyananita@gmail.com>
2024-12-10 20:17:24 +0400

    fix: trying to fix  problem with bcrypt

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,24 +1,20 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const db = require('../config/config.js');
 
-// Регистрация пользователя
 const registerUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // Проверяем, существует ли уже пользователь с таким email
-        const checkQuery = 'SELECT * FROM users WHERE email = ?';
+        const checkQuery = 'SELECT * FROM authUsers WHERE email = ?';
         const [existingUser] = await db.promise().query(checkQuery, [email]);
 
         if (existingUser.length > 0) {
             return res.status(400).json({ message: 'Пользователь с таким email уже существует' });
         }
 
-        // Хэшируем пароль
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Вставляем нового пользователя в базу данных
-        const query = 'INSERT INTO users (email, password) VALUES (?, ?)';
+        const query = 'INSERT INTO authUsers (email, password) VALUES (?, ?)';
         await db.promise().query(query, [email, hashedPassword]);
 
         res.status(201).json({ message: 'Пользователь зарегистрирован успешно' });
@@ -28,19 +24,17 @@ const registerUser = async (req, res) => {
     }
 };
 
-// Логин пользователя
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const query = 'SELECT * FROM users WHERE email = ?';
+        const query = 'SELECT * FROM authUsers WHERE email = ?';
         const [user] = await db.promise().query(query, [email]);
 
         if (user.length === 0) {
             return res.status(404).json({ message: 'Пользователь не найден' });
         }
 
-        // Сравниваем пароли
         const isMatch = await bcrypt.compare(password, user[0].password);
 
         if (!isMatch) {
