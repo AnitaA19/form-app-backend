90d13f1f6eeab70824bdc687c4c8606324246dbb
ANITA\anita <balasanyananita@gmail.com>
2024-12-10 23:43:40 +0400

    fix

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+// app.js
 const express = require('express');
-const logger = require('./middleware/logger'); 
-const parseJson = require('./middleware/parseJson'); 
+const routes = require('./routes'); // Подключение общего роутера
+const logger = require('./middleware/logger');
+const parseJson = require('./middleware/parseJson');
 
 const app = express();
 
-app.use(parseJson);
+const PORT = process.env.PORT || 3000;
 
+app.use(parseJson);
 app.use(logger);
 
-const PORT = process.env.PORT || 3000;
+app.use('/', routes);
 
 app.post('/data', (req, res) => {
     res.json({
         message: 'Data received!',
-        receivedData: req.body, 
+        receivedData: req.body,
     });
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
